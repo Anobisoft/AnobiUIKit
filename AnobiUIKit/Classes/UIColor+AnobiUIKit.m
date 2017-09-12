4f5f709c07f4d98444a2cad952bc1c37031fe0be
Stanislav Pletnev <stanislav.pletnev@academmedia.com>
2017-09-12 11:49:55 +0700

    оченьпятка

--- AnobiUIKit/Classes/UIColor+AnobiUIKit.m
+++ AnobiUIKit/Classes/UIColor+AnobiUIKit.m
@@ -11,23 +11,23 @@
 @implementation UIColor (AnobiUIKit)
 
 + (instancetype)colorWithHexString:(NSString *)string {
-    return [self colorWithHexString:string alfa:1.0];
+    return [self colorWithHexString:string alpha:1.0];
 }
 
-+ (instancetype)colorWithHexString:(NSString *)string alfa:(CGFloat)alfa {
++ (instancetype)colorWithHexString:(NSString *)string alpha:(CGFloat)alpha {
     NSString *trim = [string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
     NSString *hex = [trim stringByReplacingOccurrencesOfString:@"#" withString:@"0x"];
     NSScanner *scanner = [NSScanner scannerWithString:hex];
     unsigned rgbColor = 0;
     [scanner scanHexInt:&rgbColor];
-    return [self colorWithHexRGB:rgbColor alfa:alfa];
+    return [self colorWithHexRGB:rgbColor alpha:alpha];
 }
 
-+ (instancetype)colorWithHexRGB:(unsigned)rgbColor alfa:(CGFloat)alfa {
++ (instancetype)colorWithHexRGB:(unsigned)rgbColor alpha:(CGFloat)alpha {
     return [self colorWithRed:(CGFloat)((rgbColor & 0xFF0000) >> 0x10) / 255.0
                         green:(CGFloat)((rgbColor & 0x00FF00) >> 0x08) / 255.0
                          blue:(CGFloat)((rgbColor & 0x0000FF) >> 0x00) / 255.0
-                        alpha:alfa];
+                        alpha:alpha];
 }
 
 @end
